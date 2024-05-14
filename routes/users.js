1c797797ed5e97b23f13893f9c2c8d5470da6f69
Mehak Kamran <mehakkamran52@gmail.com>
2024-05-14 22:15:45 +0500

    setting up mongoose , model ,schema

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+const plm=require("passport-local-mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/pinterest")
+
+const userSchema=mongoose.Schema({
+  username:String,
+  name:String,
+  password:String,
+  email:String,
+  contact:Number,
+  profileImage:String,
+  board:{
+    type:Array,
+    default: []
+  }
+});
+
+userSchema.plugin(plm);
+
+const usermodel=mongoose.model("usermodel",userSchema);
+module.exports(usermodel);
